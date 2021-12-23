b944ecedfa830072a54fb25e57dbe0571d943676
Daniel Catan <danieljcatan@gmail.com>
2021-12-23 11:26:25 -0800

    loop created for DOMContentLoaded event listener that appends entries

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ function urlHandler(event) {
 photoUrl.addEventListener('input', urlHandler);
 
 var submitForm = document.querySelector('#entry-form');
+var inputs = submitForm.elements;
+var titleValue = inputs.title.value;
+var urlValue = inputs.url.value;
+var notesValue = inputs.notes.value;
 
 function submitHandler(event) {
   event.preventDefault();
-  var inputs = submitForm.elements;
-  var titleValue = inputs.title.value;
-  var urlValue = inputs.url.value;
-  var notesValue = inputs.notes.value;
   var entryValues = {
     titleValue,
     urlValue,
@@ -30,3 +30,47 @@ function submitHandler(event) {
 }
 
 submitForm.addEventListener('submit', submitHandler);
+
+function generateEntriesDOMTree(entries) {
+  var $liElement = document.createElement('li');
+  $liElement.className = 'row';
+
+  var $divColImg = document.createElement('div');
+  $divColImg.className = 'column-half img-4-entries object-contain';
+
+  var $img4Entry = document.createElement('img');
+  $img4Entry.setAttribute('src', entries.urlValue);
+
+  $liElement.appendChild($divColImg);
+  $divColImg.appendChild($img4Entry);
+
+  var $divColEntriesInfo = document.createElement('div');
+  $divColEntriesInfo.className = 'column-half entries-info';
+
+  $liElement.appendChild($divColEntriesInfo);
+
+  var $h2EntryElement = document.createElement('h2');
+  var $entryTitle = document.createTextNode(entries.titleValue);
+  $h2EntryElement.appendChild($entryTitle);
+  $divColEntriesInfo.appendChild($h2EntryElement);
+
+  var $pEntryElement = document.createElement('p');
+  var $pEntryText = document.createTextNode(entries.notesValue);
+
+  $pEntryElement.appendChild($pEntryText);
+  $divColEntriesInfo.appendChild($pEntryElement);
+
+  return $liElement;
+}
+
+var $ulEntries = document.querySelector('#entries-list');
+
+function domContentLoadedHandle(event) {
+
+  for (var i = 0; i < data.entries.length; i++) {
+    var $EntriesList = generateEntriesDOMTree(data.entries[i]);
+    $ulEntries.appendChild($EntriesList);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', domContentLoadedHandle);
